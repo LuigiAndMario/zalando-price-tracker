0325789222407b447a0cbf528442f8beaba6c956
Luigi Antoine Andrea Sansonetti <l.sansonetti@zalando.de>
2022-03-05 19:49:15 +0100

    moved stuff to price_tracker

--- price_tracker.py
+++ price_tracker.py
@@ -1,3 +1,9 @@
 import scraper
 
-scraper.scrape()
+results = scraper.scrape()
+
+for item in results:
+    output = item + ': ' + results[item][0]
+    if len(results[item]) == 2:
+        output = output + '(' + results[item][1] + ')'
+    print(output)
--- scraper.py
+++ scraper.py
@@ -11,18 +11,23 @@ def scrape():
     watchlist = json.load(f)
     f.close()
 
+    res = dict()
     for item in watchlist:
+        res[item] = list()
         page = requests.get(watchlist[item], timeout=10, headers=HEADERS)
         soup = BeautifulSoup(page.content, 'html.parser')
+
 
         prices = soup.find_all('span', string=lambda text: text and '€' in text)
         if len(prices) == 1:
             price = prices[0].string
-            output = item + ': ' + price
+            res[item].append(price)
         else:
             reduced_price = prices[0].string
-            # full_price = prices[1].string
+            #full_price = prices[1].string
             reduction = soup.find_all('span', string=lambda text: text and 'sparen' in text)[0].string.split(' ')[0]
-            output = item + ': ' + reduced_price + ' (-' + reduction + ')'
+            res[item].append(reduced_price)
+            res[item].append(reduction)
 
-        print(output)
+    
+    return res
